99ceac9321762e6ac4ec0366c3fc828ae3505b1e
Jmeow <j@jmeow.net>
2026-06-03 19:56:40 -0500

    create pretty title screen

--- game/src/main.js
+++ game/src/main.js
@@ -1,1 +1,5 @@
 import "./index.scss"
+setTimeout(() => {
+    document.getElementById("splash").classList.remove("show")
+    document.getElementById("title-screen").classList.add("show")
+}, 3000)
