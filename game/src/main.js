0a0d04d6640e8e8dd96f64f77564857ab63de133
Jmeow <j@jmeow.net>
2026-06-03 20:56:44 -0500

    indev notification

--- game/src/main.js
+++ game/src/main.js
@@ -3,3 +3,10 @@ setTimeout(() => {
     document.getElementById("splash").classList.remove("show")
     document.getElementById("title-screen").classList.add("show")
 }, 3000)
+if (sessionStorage.getItem("indev-notif-dismissed") == "true") {
+    document.getElementById("indev-notif").style.display = "none"
+}
+document.getElementById("indev-notif").addEventListener("click", () => {
+    document.getElementById("indev-notif").classList.add("dismissed")
+    sessionStorage.setItem("indev-notif-dismissed", "true")
+})
