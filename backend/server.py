dcfece738a7d3663248beb8b0830cfe930826100
Jmeow <j@jmeow.net>
2026-06-10 20:46:12 -0500

    add server route for running spkpos function

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,33 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs, urlsplit
+
+import spiceypy as spice
+
+spice.furnsh("./de430.bsp")
+spice.furnsh("./latest_leapseconds.tls")
 
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(req):
-        if(req.path == "/health"):
+        url = urlsplit(req.path)
+        query = parse_qs(url.query)
+        if(url.path == "/health"):
             req.send_response(200)
             req.send_header("Access-Control-Allow-Origin", "*")
             req.send_header("Content-Type", "application/json")
             req.end_headers()
             req.wfile.write(bytes(json.dumps({"up": True}), "utf-8"))
+        elif(url.path == "/spkpos"):
+            print(url)
+            print(query)
+            result = spice.spkpos(query["targ"][0], spice.str2et(query["time"][0]), "J2000", "NONE", query["obs"][0])
+            print(result)
+            req.send_response(200)
+            req.send_header("Access-Control-Allow-Origin", "*")
+            req.send_header("Content-Type", "application/json")
+            req.end_headers()
+            req.wfile.write(bytes(json.dumps({"ok": True, "result": {"x": result[0][0], "y": result[0][1], "z": result[0][2]}}), "utf-8"))
         else:
             req.send_response(404)
             req.end_headers()
