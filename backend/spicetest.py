7ea29baca65dc80642f0735dc6923450f2565ca9
Jmeow <j@jmeow.net>
2026-06-14 00:22:47 -0500

    delete spice code and backend because i am switching back to gmat
    it will live on in git history

--- backend/spicetest.py
+++
@@ -1,9 +0,0 @@
-import spiceypy as spice
-
-print(spice.tkvrsn('TOOLKIT'))
-
-spice.furnsh("./de430.bsp")
-spice.furnsh("./latest_leapseconds.tls")
-timstr = '2007 JAN 1 00:00:00'
-et = spice.str2et(timstr)
-print(spice.spkpos("moon", et, "J2000", "NONE", "earth"))
