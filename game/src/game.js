26755b64161786e1f8f4376028ce029d10087623
Jmeow <j@jmeow.net>
2026-06-26 18:16:02 -0500

    better sphere

--- game/src/game.js
+++ game/src/game.js
@@ -45,72 +45,101 @@ export class Game {
     }
     draw() {
         this.ctx.clearRect(0, 0, this.width, this.height)
-        this.ctx.fillStyle = "#ffeecc"
+        this.ctx.fillStyle = "#000000aa"
         this.ctx.strokeStyle = "#ffeecc"
         this.ctx.lineWidth = 2
-        const origin = this.project(...this.camTransform(0, 0, 0))
-        this.ctx.beginPath()
-        this.ctx.ellipse(
-            ...origin,
-            Math.abs(
-                this.project(...this.camTransform(50, 0, 0))[0] - origin[0],
-            ),
-            Math.abs(
-                this.project(...this.camTransform(0, 0, 50))[1] - origin[1],
-            ),
-            0,
-            0,
-            Math.PI * 2,
-        )
-        this.ctx.stroke()
-        this.ctx.beginPath()
-        this.ctx.ellipse(
-            ...origin,
-            Math.abs(
-                this.project(...this.camTransform(0, 0, 50))[0] - origin[0],
-            ),
-            Math.abs(
-                this.project(...this.camTransform(0, 50, 0))[1] - origin[1],
-            ),
-            0,
-            0,
-            Math.PI * 2,
-        )
-        this.ctx.stroke()
-        this.ctx.beginPath()
-        this.ctx.ellipse(
-            ...origin,
-            Math.abs(
-                this.project(...this.camTransform(50, 0, 0))[0] - origin[0],
-            ),
-            Math.abs(
-                this.project(...this.camTransform(0, 50, 0))[1] - origin[1],
-            ),
-            0,
-            0,
-            Math.PI * 2,
-        )
-        this.ctx.stroke()
-        this.ctx.beginPath()
-        this.ctx.moveTo(...this.project(...this.camTransform(-10, 10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(10, 10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(10, -10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(-10, -10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(-10, 10, 10)))
-        this.ctx.moveTo(...this.project(...this.camTransform(-10, 10, -10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(10, 10, -10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(10, -10, -10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(-10, -10, -10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(-10, 10, -10)))
-        this.ctx.moveTo(...this.project(...this.camTransform(-10, 10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(-10, 10, -10)))
-        this.ctx.moveTo(...this.project(...this.camTransform(10, 10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(10, 10, -10)))
-        this.ctx.moveTo(...this.project(...this.camTransform(10, -10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(10, -10, -10)))
-        this.ctx.moveTo(...this.project(...this.camTransform(-10, -10, 10)))
-        this.ctx.lineTo(...this.project(...this.camTransform(-10, -10, -10)))
-        this.ctx.stroke()
+        const p = function p(x, y, z) {
+            return this.project(...this.camTransform(x, y, z))
+        }.bind(this)
+        const drawList = []
+        const origin = p(0, 0, 0)
+        drawList.push({
+            z: -50,
+            func: () => {
+                this.ctx.beginPath()
+                this.ctx.ellipse(
+                    ...origin,
+                    Math.abs(p(50, 0, 0)[0] - origin[0]),
+                    Math.abs(p(0, 0, 50)[1] - origin[1]),
+                    0,
+                    p(0, 0, 50)[1] - origin[1] < 0 ? 0 : Math.PI,
+                    p(0, 0, 50)[1] - origin[1] < 0 ? Math.PI : Math.PI * 2,
+                )
+                this.ctx.stroke()
+            },
+        })
+        drawList.push({
+            z: 50,
+            func: () => {
+                this.ctx.beginPath()
+                this.ctx.ellipse(
+                    ...origin,
+                    Math.abs(p(50, 0, 0)[0] - origin[0]),
+                    Math.abs(p(0, 0, 50)[1] - origin[1]),
+                    0,
+                    p(0, 0, 50)[1] - origin[1] > 0 ? 0 : Math.PI,
+                    p(0, 0, 50)[1] - origin[1] > 0 ? Math.PI : Math.PI * 2,
+                )
+                this.ctx.stroke()
+            },
+        })
+        drawList.push({
+            z: -50,
+            func: () => {
+                this.ctx.beginPath()
+                this.ctx.ellipse(
+                    ...origin,
+                    Math.abs(p(0, 0, 50)[0] - origin[0]),
+                    Math.abs(p(0, 50, 0)[1] - origin[1]),
+                    0,
+                    p(0, 0, 50)[0] - origin[0] > 0
+                        ? Math.PI / 2
+                        : (Math.PI / 2) * 3,
+                    p(0, 0, 50)[0] - origin[0] > 0
+                        ? (Math.PI / 2) * 3
+                        : (Math.PI / 2) * 5,
+                )
+                this.ctx.stroke()
+            },
+        })
+        drawList.push({
+            z: 50,
+            func: () => {
+                this.ctx.beginPath()
+                this.ctx.ellipse(
+                    ...origin,
+                    Math.abs(p(0, 0, 50)[0] - origin[0]),
+                    Math.abs(p(0, 50, 0)[1] - origin[1]),
+                    0,
+                    p(0, 0, 50)[0] - origin[0] > 0 ? -Math.PI / 2 : Math.PI / 2,
+                    p(0, 0, 50)[0] - origin[0] > 0
+                        ? Math.PI / 2
+                        : (Math.PI / 2) * 3,
+                )
+                this.ctx.stroke()
+            },
+        })
+        drawList.push({
+            z: 0,
+            func: () => {
+                this.ctx.beginPath()
+                this.ctx.ellipse(
+                    ...origin,
+                    Math.abs(p(50, 0, 0)[0] - origin[0]),
+                    Math.abs(p(0, 50, 0)[1] - origin[1]),
+                    0,
+                    0,
+                    Math.PI * 2,
+                )
+                this.ctx.fill()
+                this.ctx.stroke()
+            },
+        })
+        drawList
+            .sort((a, b) => b.z - a.z)
+            .forEach((item) => {
+                item.func()
+            })
         requestAnimationFrame(this.draw.bind(this))
     }
 }
