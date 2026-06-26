32a65ee75d383895aeda1a434548b76117abff7f
Jmeow <j@jmeow.net>
2026-06-26 12:29:40 -0500

    3d cube

--- game/src/game.js
+++ game/src/game.js
@@ -1,10 +1,26 @@
+const focalLength = 300
+
 export class Game {
+    camera = {
+        translate: {
+            x: 0,
+            y: 0,
+            z: 100,
+        },
+    }
     constructor(canvas) {
         this.canvas = canvas
         this.ctx = this.canvas.getContext("2d")
         this.resize()
         addEventListener("resize", this.resize.bind(this))
         this.draw()
+        this.canvas.addEventListener("mousemove", this.mouseMove.bind(this))
+    }
+    mouseMove(ev) {
+        if (ev.buttons & 1) {
+            this.camera.translate.x += ev.movementX
+            this.camera.translate.y += ev.movementY
+        }
     }
     resize() {
         this.canvas.width = innerWidth * devicePixelRatio
@@ -14,10 +30,44 @@ export class Game {
         this.ctx.resetTransform()
         this.ctx.scale(devicePixelRatio, devicePixelRatio)
     }
+    project(x, y, z) {
+        return [
+            x * (focalLength / z) + this.width / 2,
+            y * (focalLength / z) + this.height / 2,
+        ]
+    }
+    camTransform(x, y, z) {
+        return [
+            x + this.camera.translate.x,
+            y + this.camera.translate.y,
+            z + this.camera.translate.z,
+        ]
+    }
     draw() {
         this.ctx.clearRect(0, 0, this.width, this.height)
         this.ctx.fillStyle = "#ffeecc"
-        this.ctx.fillRect(this.width / 2 - 100, this.height / 2 - 100, 200, 200)
+        this.ctx.strokeStyle = "#ffeecc"
+        this.ctx.lineWidth = 2
+        this.ctx.beginPath()
+        this.ctx.moveTo(...this.project(...this.camTransform(-10, 10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(10, 10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(10, -10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(-10, -10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(-10, 10, 10)))
+        this.ctx.moveTo(...this.project(...this.camTransform(-10, 10, -10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(10, 10, -10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(10, -10, -10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(-10, -10, -10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(-10, 10, -10)))
+        this.ctx.moveTo(...this.project(...this.camTransform(-10, 10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(-10, 10, -10)))
+        this.ctx.moveTo(...this.project(...this.camTransform(10, 10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(10, 10, -10)))
+        this.ctx.moveTo(...this.project(...this.camTransform(10, -10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(10, -10, -10)))
+        this.ctx.moveTo(...this.project(...this.camTransform(-10, -10, 10)))
+        this.ctx.lineTo(...this.project(...this.camTransform(-10, -10, -10)))
+        this.ctx.stroke()
         requestAnimationFrame(this.draw.bind(this))
     }
 }
